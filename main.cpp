a9725d3a2e0e679490d03599dab37870031a31c5
gabrielpazinatto <gabrielrpazinatto@gmail.com>
2025-01-05 22:58:32 -0300

    Added segmentation-based swatches

--- main.cpp
+++ main.cpp
@@ -5,260 +5,221 @@
 #include <vector>
 #include <stack>
 #include <omp.h>
+#include <queue>
 
 class Swatch
 {
 public:
-    cv::Point point1_reference{-1, -1};
-    cv::Point point2_reference{-1, -1};
-    cv::Point point1_grayscale{-1, -1};
-    cv::Point point2_grayscale{-1, -1};
+    std::vector<cv::Vec3b*> grayscale_pixels;
+    std::vector<cv::Vec3b*> reference_pixels; 
     Swatch()
     {
     }
 };
+
+void region_growing(cv::Mat img, std::vector<cv::Vec3b*>& pixels, cv::Point seed, int threshold, int channel = 1){
+
+    const int CONNECTIVITY = 4;
+    const int dx[CONNECTIVITY] = {-1, 1, 0, 0};
+    const int dy[CONNECTIVITY] = {0, 0, -1, 1};
+
+    pixels.clear();
+
+    cv::Mat visited = cv::Mat::zeros(img.size(), CV_8U);
+    int seed_value = img.at<cv::Vec3b>(seed.y, seed.x)[channel];
+
+    std::queue<cv::Point> to_visit;
+    to_visit.push(seed);
+
+    visited.at<uchar>(seed.y, seed.x) = 1;
+
+    int i = 0;
+    while(!to_visit.empty()){
+        cv::Point current = to_visit.front();
+        to_visit.pop();
+
+        pixels.push_back(&img.at<cv::Vec3b>(current.y, current.x));
+        
+        for(int i = 0; i < CONNECTIVITY; i++){
+            int new_x = current.x + dx[i];
+            int new_y = current.y + dy[i];
+
+            if(!(new_x >= 0 && new_x < img.cols && new_y >= 0 && new_y < img.rows && !visited.at<uchar>(new_y, new_x))){ 
+                continue;
+            }
+
+            cv::Vec3b pixel = img.at<cv::Vec3b>(new_y, new_x);
+            int diff = abs(seed_value - pixel[channel]);
+
+            if(diff <= threshold){
+                visited.at<uchar>(new_y, new_x) = 1;
+                to_visit.push(cv::Point(new_x, new_y));
+            }
+        }
+    }
+}
 
 // Converts an image from BGR to LAB color space
 void convert_bgr_to_lab(cv::Mat &img)
 {
     cv::cvtColor(img, img, cv::COLOR_BGR2Lab);
 }
-// Gets the average luminance of a window in an image
-float get_average_luminance(const cv::Mat img, const cv::Point point1, const cv::Point point2)
-{
-    float luminance_sum = 0;
-    int start_row = point1.y > point2.y ? point2.y : point1.y;
-    int end_row = point2.y > point1.y ? point2.y : point1.y;
-    int start_col = point1.x > point2.x ? point2.x : point1.x;
-    int end_col = point2.x > point1.x ? point2.x : point1.x;
 
-    for (int i = start_row; i < end_row; i++)
-    {
-        for (int j = start_col; j < end_col; j++)
-        {
-            if (i < img.rows && j < img.cols)
-            {
-                luminance_sum += img.at<cv::Vec3b>(i, j)[0];
-            }
-        }
+float get_average_luminance(std::vector<cv::Vec3b*> pixels){
+    float sum = 0;
+    for(cv::Vec3b* pixel : pixels){
+        sum += (*pixel)[0];
     }
-    return luminance_sum / ((end_row - start_row) * (end_col - start_col));
+    return sum / pixels.size();
 }
 
-// Gets the standard deviation of the luminance of a window in an image
-float get_luminance_std_dev(const cv::Mat img, const cv::Point point1, const cv::Point point2, const float average_luminance)
-{
-    float std_dev = 0;
-    int start_row = point1.y > point2.y ? point2.y : point1.y;
-    int end_row = point2.y > point1.y ? point2.y : point1.y;
-    int start_col = point1.x > point2.x ? point2.x : point1.x;
-    int end_col = point2.x > point1.x ? point2.x : point1.x;
-
-    for (int i = start_row; i < end_row; i++)
-    {
-        for (int j = start_col; j < end_col; j++)
-        {
-            if (i < img.rows && j < img.cols)
-            {
-                std_dev += pow(img.at<cv::Vec3b>(i, j)[0] - average_luminance, 2);
-            }
-        }
+float get_std_dev_luminance(std::vector<cv::Vec3b*> pixels, float average){
+    float sum = 0;
+    for(cv::Vec3b* pixel : pixels){
+        sum += pow((*pixel)[0] - average, 2);
     }
-    return sqrt(std_dev / ((end_row - start_row) * (end_col - start_col) - 1));
+    return sqrt(sum / (pixels.size() - 1));
 }
 
 // Gets the index of the sample whose luminance is the closest to the given luminance
 // Using binary search could affect the results?
-int closest_match_index(int lum, std::vector<cv::Vec3b> samples)
+int closest_match_index(int lum, std::vector<cv::Vec3b*> samples)
 {
-
+    int left = 0;
+    int right = samples.size() - 1;
     int closest_index = 0;
     int closest_distance = 255;
 
-    for (int i = 0; i < samples.size(); i++)
+    while (left <= right)
     {
-        int distance = abs(samples[i][0] - lum);
+        int mid = left + (right - left) / 2;
+        int sample_lum = (*samples[mid])[0];
+        int distance = abs(sample_lum - lum);
+
         if (distance < closest_distance)
         {
             closest_distance = distance;
-            closest_index = i;
+            closest_index = mid;
+        }
+
+        if (sample_lum < lum)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
         }
     }
+
     return closest_index;
 }
 
 // Matches the luminance of the source image to the destination image
 // Similar to histogram matching, but is a linear transformation that uses neighborhood statistics
-void remap_luminance(const cv::Mat dst, cv::Mat &src, Swatch swatch)
+void remap_luminance(Swatch swatch)
 {
-    float average_dst_luminance = get_average_luminance(dst, swatch.point1_grayscale, swatch.point2_grayscale);
-    float dst_std_dev = get_luminance_std_dev(dst, swatch.point1_grayscale, swatch.point2_grayscale, average_dst_luminance);
-    float average_src_luminance = get_average_luminance(src, swatch.point1_reference, swatch.point2_reference);
-    float src_std_dev = get_luminance_std_dev(src, swatch.point1_reference, swatch.point2_reference, average_src_luminance);
-
-    int start_row = swatch.point1_reference.y > swatch.point2_reference.y ? swatch.point2_reference.y : swatch.point1_reference.y;
-    int end_row = swatch.point2_reference.y > swatch.point1_reference.y ? swatch.point2_reference.y : swatch.point1_reference.y;
-    int start_col = swatch.point1_reference.x > swatch.point2_reference.x ? swatch.point2_reference.x : swatch.point1_reference.x;
-    int end_col = swatch.point2_reference.x > swatch.point1_reference.x ? swatch.point2_reference.x : swatch.point1_reference.x;
+    float average_dst_luminance = get_average_luminance(swatch.grayscale_pixels);
+    float dst_std_dev = get_std_dev_luminance(swatch.grayscale_pixels, average_dst_luminance);
+    float average_src_luminance = get_average_luminance(swatch.reference_pixels);
+    float src_std_dev = get_std_dev_luminance(swatch.reference_pixels, average_src_luminance);
 
     int lum;
-
-    for (int i = start_row; i < end_row; i++)
-    {
-        for (int j = start_col; j < end_col; j++)
-        { 
-            lum = src.at<cv::Vec3b>(i, j)[0];
-            src.at<cv::Vec3b>(i, j)[0] = cv::saturate_cast<signed char>((lum - average_src_luminance) * (dst_std_dev / src_std_dev) + average_dst_luminance);
-        }
+    #pragma omp parallel for
+    for (int i = 0; i < swatch.reference_pixels.size(); i++)
+    { 
+        lum = (*swatch.reference_pixels[i])[0];
+        (*swatch.reference_pixels[i])[0] = cv::saturate_cast<signed char>((lum - average_src_luminance) * (dst_std_dev / src_std_dev) + average_dst_luminance);
     }
+    
 }
 
-// effectively matches the colors of the source image to the destination image
-void match_colors_in_swatch(std::vector<cv::Vec3b> samples, cv::Mat dst, cv::Point point1, cv::Point point2)
-{
-    int start_row = point1.y > point2.y ? point2.y : point1.y;
-    int end_row = point2.y > point1.y ? point2.y : point1.y;
-    int start_col = point1.x > point2.x ? point2.x : point1.x;
-    int end_col = point2.x > point1.x ? point2.x : point1.x;
-
-    for (int i = start_row; i < end_row; i++)
+void match_colors_in_swatch(std::vector<cv::Vec3b*> samples, cv::Mat dst, Swatch swatch){
+    for (int i = 0; i < swatch.grayscale_pixels.size(); i++)
     {
-        for (int j = start_col; j < end_col; j++)
-        {
-            int lum = dst.at<cv::Vec3b>(i, j)[0];
-            int closest_index = closest_match_index(lum, samples);
-            dst.at<cv::Vec3b>(i, j)[1] = samples[closest_index][1];
-            dst.at<cv::Vec3b>(i, j)[2] = samples[closest_index][2];
-        }
+        int lum = (*swatch.grayscale_pixels[i])[0];
+        int closest_index = closest_match_index(lum, samples);
+        (*swatch.grayscale_pixels[i])[1] = (*samples[closest_index])[1];
+        (*swatch.grayscale_pixels[i])[2] = (*samples[closest_index])[2];
     }
 }
 
 // Gets a jittered sample of the image
 // Separates the image into windows and gets a random pixel from each window
-void get_swatch_jittered_sample(const cv::Mat src, std::vector<cv::Vec3b> &samples, const Swatch swatch, const int window_size = 5)
+void get_jittered_samples(const cv::Mat src, std::vector<cv::Vec3b*>& samples, Swatch swatch, const int window_size = 5)
 {
-    int start_row = swatch.point1_reference.y > swatch.point2_reference.y ? swatch.point2_reference.y : swatch.point1_reference.y;
-    int end_row = swatch.point2_reference.y > swatch.point1_reference.y ? swatch.point2_reference.y : swatch.point1_reference.y;
-    int start_col = swatch.point1_reference.x > swatch.point2_reference.x ? swatch.point2_reference.x : swatch.point1_reference.x;
-    int end_col = swatch.point2_reference.x > swatch.point1_reference.x ? swatch.point2_reference.x : swatch.point1_reference.x;
-
-    int x, y;
-    for (int i = start_row; i < end_row; i += window_size)
+    int x;
+    for (int i = 0; i < swatch.reference_pixels.size(); i += window_size)
     {
-        for (int j = start_col; j < end_col; j += window_size)
+        if (i + window_size >= swatch.reference_pixels.size())
         {
-            if (i + window_size >= end_row)
-            {
-                x = i + rand() % (end_row - i);
-            }
-            else
-            {
-                x = i + rand() % window_size;
-            }
-
-            if (j + window_size >= end_col)
-            {
-                y = j + rand() % (end_col - j);
-            }
-            else
-            {
-                y = j + rand() % window_size;
-            }
-
-            samples.push_back(src.at<cv::Vec3b>(x, y));
+            x = i + rand() % (swatch.reference_pixels.size() - i);
         }
+        else
+        {
+            x = i + rand() % window_size;
+        }
+
+        samples.push_back(swatch.reference_pixels[x]);
     }
-    sort(samples.begin(), samples.end(), [](cv::Vec3b a, cv::Vec3b b)
-         { return a[0] < b[0]; });
+    sort(samples.begin(), samples.end(), [](cv::Vec3b* a, cv::Vec3b* b)
+        { return (*a)[0] < (*b)[0]; });
 }
 
 std::vector<Swatch> swatches;
 Swatch *current_swatch = new Swatch();
+int threshold = 0;
 
 void mouseCallbackReference(int event, int x, int y, int, void *userdata)
 {
-    cv::Mat &image = *(cv::Mat *)userdata;
-    static bool point1_set = false;
-    static bool point2_set = false;
+    cv::Mat &image = *(cv::Mat*)userdata;
+    static cv::Mat image_clone = image.clone();
+    static std::vector<cv::Vec3b*> clone_samples;
 
-    if (event == cv::EVENT_LBUTTONDOWN)
+    if(event == cv::EVENT_LBUTTONDOWN)
     {
-        if (!point1_set)
+        region_growing(image, current_swatch->reference_pixels, cv::Point(x, y), threshold, 0);
+        region_growing(image_clone, clone_samples, cv::Point(x, y), threshold, 0);
+
+        std::cout << "Reference pixels: " << current_swatch->reference_pixels.size() << std::endl;
+
+        for(cv::Vec3b* pixel : clone_samples)
         {
-            current_swatch->point1_reference = cv::Point(x, y);
-            point1_set = true;
+            *pixel = {0, 0, 0};
         }
-        else if (!point2_set)
-        {
-            current_swatch->point2_reference = cv::Point(x, y);
-            point2_set = true;
+        cv::imshow("Reference Image", image_clone);
 
-            // Gerar uma cor aleatória
-            std::random_device rd;
-            std::mt19937 gen(rd());
-            std::uniform_int_distribution<> dist(0, 255);
-            cv::Scalar randomColor(dist(gen), dist(gen), dist(gen));
-
-            // Colorir os pixels entre os dois pontos
-            cv::rectangle(image, current_swatch->point1_reference, current_swatch->point2_reference, randomColor, 2);
-            cv::imshow("Reference Image", image);
-        }
-
-        if (current_swatch->point1_grayscale != (cv::Point){-1, -1} && current_swatch->point2_grayscale != (cv::Point){-1, -1} && current_swatch->point1_reference != (cv::Point){-1, -1} && current_swatch->point2_reference != (cv::Point){-1, -1})
+        if(current_swatch->reference_pixels.size() > 0 && current_swatch->grayscale_pixels.size() > 0)
         {
             swatches.push_back(*current_swatch);
             current_swatch = new Swatch();
-        }
-
-        // Resetar os pontos
-        if (point1_set && point2_set)
-        {
-            point1_set = false;
-            point2_set = false;
         }
     }
 }
 
 void mouseCallbackGrayscale(int event, int x, int y, int, void *userdata)
 {
-    cv::Mat &image = *(cv::Mat *)userdata;
-    static bool point1_set = false;
-    static bool point2_set = false;
+    cv::Mat &image = *(cv::Mat*)userdata;
+    static cv::Mat image_clone = image.clone();
+    static std::vector<cv::Vec3b*> clone_samples;
 
-    if (event == cv::EVENT_LBUTTONDOWN)
+    if(event == cv::EVENT_LBUTTONDOWN)
     {
-        if (!point1_set)
+        region_growing(image, current_swatch->grayscale_pixels, cv::Point(x, y), threshold, 0);
+        region_growing(image_clone, clone_samples, cv::Point(x, y), threshold, 0);
+
+        std::cout << "Grayscale pixels: " << current_swatch->grayscale_pixels.size() << std::endl;
+
+        for(cv::Vec3b* pixel : clone_samples)
         {
-            current_swatch->point1_grayscale = cv::Point(x, y);
-            point1_set = true;
+            *pixel = {0, 0, 0};
         }
-        else if (!point2_set)
-        {
-            current_swatch->point2_grayscale = cv::Point(x, y);
-            point2_set = true;
+        cv::imshow("Grayscale Image", image_clone);
 
-            // Gerar uma cor aleatória
-            std::random_device rd;
-            std::mt19937 gen(rd());
-            std::uniform_int_distribution<> dist(0, 255);
-            cv::Scalar randomColor(dist(gen), dist(gen), dist(gen));
-
-            // Colorir os pixels entre os dois pontos
-            cv::rectangle(image, current_swatch->point1_grayscale, current_swatch->point2_grayscale, randomColor, 2);
-            cv::imshow("Grayscale Image", image);
-        }
-
-        if (current_swatch->point1_grayscale != (cv::Point){-1, -1} && current_swatch->point2_grayscale != (cv::Point){-1, -1} && current_swatch->point1_reference != (cv::Point){-1, -1} && current_swatch->point2_reference != (cv::Point){-1, -1})
+        if(current_swatch->reference_pixels.size() > 0 && current_swatch->grayscale_pixels.size() > 0)
         {
             swatches.push_back(*current_swatch);
             current_swatch = new Swatch();
-        }
-
-        // Resetar os pontos
-        if (point1_set && point2_set)
-        {
-            point1_set = false;
-            point2_set = false;
         }
     }
 }
@@ -271,14 +232,14 @@ int main(int argc, char *argv[])
 
     if (argc < 4)
     {
-        std::cerr << "Usage: " << argv[0] << " <grayscale_image> <reference_image> <window_size>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <grayscale_image> <reference_img> <window_size>" << std::endl;
         return -1;
     }
 
     cv::Mat grayscale_img = cv::imread(argv[1]);
-    cv::Mat reference_image = cv::imread(argv[2]);
+    cv::Mat reference_img = cv::imread(argv[2]);
 
-    if (grayscale_img.empty() || reference_image.empty())
+    if (grayscale_img.empty() || reference_img.empty())
     {
         std::cerr << "Error: Could not open or find the images!" << std::endl;
         return -1;
@@ -287,13 +248,14 @@ int main(int argc, char *argv[])
     cv::Mat res = grayscale_img.clone();
 
     cv::imshow("Grayscale Image", grayscale_img);
-    cv::imshow("Reference Image", reference_image);
+    cv::imshow("Reference Image", reference_img);
 
     cv::Mat grayscale_clone = grayscale_img.clone();
-    cv::Mat reference_clone = reference_image.clone();
+    cv::Mat reference_clone = reference_img.clone();
 
-    cv::setMouseCallback("Reference Image", mouseCallbackReference, &reference_clone);
-    cv::setMouseCallback("Grayscale Image", mouseCallbackGrayscale, &grayscale_clone);
+    cv::setMouseCallback("Reference Image", mouseCallbackReference, &reference_img);
+    cv::setMouseCallback("Grayscale Image", mouseCallbackGrayscale, &grayscale_img);
+    cv::createTrackbar("Threshold", "Grayscale Image", &threshold, 255);
 
     while (cv::waitKey(0) != 32)
     {
@@ -302,37 +264,32 @@ int main(int argc, char *argv[])
 
     std::cout << "Swatches: " << swatches.size() << std::endl;
 
-    for (Swatch s : swatches)
-    {
-        std::cout << s.point1_grayscale << "," << s.point2_grayscale << "," << s.point1_reference << "," << s.point2_reference << std::endl;
-    }
-
     convert_bgr_to_lab(grayscale_img);
-    convert_bgr_to_lab(reference_image);
+    convert_bgr_to_lab(reference_img);
     convert_bgr_to_lab(res);
     
+    int i = 0;
     std::cout << "Remapping luminance..." << std::endl;
-    for(Swatch s : swatches)
+    for(Swatch swatch : swatches)
     {
-        remap_luminance(grayscale_img, reference_image, s);
-        std::cout << "loop " << std::endl;
+        remap_luminance(swatch);
     }
  
-    std::cout << "Sampling and matching colors..." << std::endl;
-    std::vector<cv::Vec3b> samples;
-    for(Swatch s : swatches)
+    std::cout << "Getting Samples and Matching Colors..." << std::endl;
+
+    std::vector<cv::Vec3b*> samples;
+    for(Swatch swatch : swatches)
     {
-        get_swatch_jittered_sample(reference_image, samples, s, atoi(argv[3]));
-        match_colors_in_swatch(samples, res, s.point1_grayscale, s.point2_grayscale);
+        get_jittered_samples(reference_img, samples, swatch, atoi(argv[3]));
+        match_colors_in_swatch(samples, res, swatch);
         samples.erase(samples.begin(), samples.end());
     }
 
-    std::cout << "Matching colors..." << std::endl;
+    cv::cvtColor(reference_img, reference_img, cv::COLOR_Lab2BGR);
+    cv::imshow("Reference Image", reference_img);
 
-    cv::cvtColor(res, res, cv::COLOR_Lab2BGR);
-    cv::imwrite("result.jpg", res);
-
-    cv::imshow("Result", res);
+    cv::cvtColor(grayscale_img, grayscale_img, cv::COLOR_Lab2BGR);
+    cv::imshow("Grayscale Image", grayscale_img);
 
     app.exec();
     return 0;
